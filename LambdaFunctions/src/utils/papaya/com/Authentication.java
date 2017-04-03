a661768d35860934f8a96c48e778cf9f1b05b919
Adam Johnston <johnst63@purdue.edu>
2017-04-03 02:52:53 -0400

    Changed Authentication.FACEBOOK_KEY_MIN_LEN to 17

--- LambdaFunctions/src/utils/papaya/com/Authentication.java
+++ LambdaFunctions/src/utils/papaya/com/Authentication.java
@@ -6,7 +6,7 @@ public class Authentication {
 	public static final String SERVICE_GOOGLE = "GOOGLE";
 	
 	public static final int FACEBOOK_KEY_MAX_LEN = 45;
-	public static final int FACEBOOK_KEY_MIN_LEN = 20;
+	public static final int FACEBOOK_KEY_MIN_LEN = 17;
 	public static final int GOOGLE_KEY_MAX_LEN = 45;
 	public static final int GOOGLE_KEY_MIN_LEN = 20;
 	
