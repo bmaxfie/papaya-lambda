acbeddaa1415be1734b19a7230ee3958473e1f06
johnst63 <johnst63@purdue.edu>
2017-04-05 01:13:10 -0400

    Created Google Authorization and isAuthorized method (needs testing and facebook)

--- LambdaFunctions/src/utils/papaya/com/Authentication.java
+++ LambdaFunctions/src/utils/papaya/com/Authentication.java
@@ -1,5 +1,13 @@
 package utils.papaya.com;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+
 public class Authentication {
 	
 	public static final String SERVICE_FACEBOOK = "FACEBOOK";
@@ -8,4 +16,17 @@ public class Authentication {
 	public static final int FACEBOOK_KEY_MIN_LEN = 17;
 	public static final int GOOGLE_KEY_MIN_LEN = 20;
 	
+	public static boolean isAuthorized(String authentication_key, String service_user_id, AuthServiceType service_type) throws GeneralSecurityException, IOException {
+		if(service_type == AuthServiceType.GOOGLE) {
+			GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), JacksonFactory.getDefaultInstance())
+				    .setAudience(Collections.singletonList(service_user_id)).build();
+			if(verifier.verify(authentication_key) != null) {
+				return true;
+			}
+		} else if(service_type == AuthServiceType.FACEBOOK) {
+			
+		}
+		return false;
+	}
 }
+
