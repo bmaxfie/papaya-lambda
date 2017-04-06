0ee6c933252536ee587ec98c598e561d040950b5
Adam Johnston <johnst63@purdue.edu>
2017-04-06 01:54:00 -0400

    Added more to facebook auth

--- LambdaFunctions/src/utils/papaya/com/Authentication.java
+++ LambdaFunctions/src/utils/papaya/com/Authentication.java
@@ -1,6 +1,8 @@
 package utils.papaya.com;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Authenticator;
 import java.net.InetSocketAddress;
 import java.net.MalformedURLException;
@@ -12,6 +14,8 @@ import java.util.Collections;
 
 import javax.net.ssl.HttpsURLConnection;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
@@ -27,7 +31,7 @@ public class Authentication {
 	public static final int FACEBOOK_KEY_MIN_LEN = 17;
 	public static final int GOOGLE_KEY_MIN_LEN = 20;
 	
-	public static boolean isAuthorized(String authentication_key, String service_user_id, AuthServiceType service_type) throws GeneralSecurityException, IOException {
+	public static boolean isAuthorized(String authentication_key, String service_user_id, AuthServiceType service_type) throws GeneralSecurityException, IOException, JSONException {
 		if(service_type == AuthServiceType.GOOGLE) {
 			GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), JacksonFactory.getDefaultInstance())
 				    .setAudience(Collections.singletonList(service_user_id)).build();
@@ -35,14 +39,22 @@ public class Authentication {
 				return true;
 			}
 		} else if(service_type == AuthServiceType.FACEBOOK) {
-			String proxyName;
-			String url_name = "https://www.facebook.com/";
-			int port;
+
+			String url_name = "https://graph.facebook.com/" + service_user_id + "/access_token=" + authentication_key;
+			
 			try {
-				
+				StringBuilder result = new StringBuilder();
 				URL url = new URL(url_name);
 				HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
-			
+				con.setRequestMethod("GET");
+				BufferedReader rd = new BufferedReader(new InputStreamReader(con.getInputStream()));
+				String line;
+				while((line = rd.readLine()) != null) {
+					result.append(line);
+				}
+				rd.close();
+				JSONObject fb_return = new JSONObject(result);
+				fb_return.getString("access_token");
 
 	    } catch (MalformedURLException e) {
 	        e.printStackTrace();
